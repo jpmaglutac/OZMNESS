513d329e8b703b867fa741c0e081ea4f88a2be1a
pepe <pepe@it.manila.orangeandbronze.com>
2011-07-08 14:32:29 +0800

    Created ratingService and tests for RatingService

---
+++ test/unit/com/orangeandbronze/ozmness/RatingServiceTests.groovy
@@ -0,0 +1,41 @@
+package com.orangeandbronze.ozmness
+
+import grails.test.*
+
+class RatingServiceTests extends GrailsUnitTestCase {
+	
+	def ratingService
+	
+	Employee higher
+	Employee lower
+	
+    protected void setUp() {
+        super.setUp()
+		ratingService = new RatingService()
+		EmployeePosition position = new EmployeePosition(name: "dev", recommendedRating: 2.0)
+		higher = new Employee(username: "higher", password: "pass", enabled: true, accountExpired: false,
+			accountLocked: false, passwordExpired: false, position: position)
+		lower = new Employee(username: "lower", password: "pass", enabled: true, accountExpired: false, 
+            accountLocked: false, passwordExpired: false, position: position, mentor: higher)
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSameEmployeeOnIsSameEmployeeReturnsTrue() {
+		assertTrue(ratingService.isSameEmployee(lower, lower))
+    }
+	
+	void testDifferentEmployeeOnIsSameEmployeeReturnsFalse() {
+		assertFalse(ratingService.isSameEmployee(higher, lower))
+	}
+	
+	void testMentorOnIsMentorReturnsTrue() {
+		assertTrue(ratingService.isMentor(higher, lower))
+	}
+	
+	void testNotMentorOnIsMentorReturnsFalse() {
+		assertFalse(ratingService.isMentor(lower, lower))
+	}
+}
