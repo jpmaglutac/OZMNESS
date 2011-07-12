737c0203341e9a9eb67e12361048c462b4de16db
pepe <pepe@it.manila.orangeandbronze.com>
2011-07-12 16:11:03 +0800

    Created test for RatingService

--- test/unit/com/orangeandbronze/ozmness/RatingServiceTests.groovy
+++ test/unit/com/orangeandbronze/ozmness/RatingServiceTests.groovy
@@ -9,11 +9,12 @@ class RatingServiceTests extends GrailsUnitTestCase {
 	Employee higher
 	Employee lower
 	Employee mid
+	EmployeePosition position
 	
     protected void setUp() {
         super.setUp()
 		ratingService = new RatingService()
-		EmployeePosition position = new EmployeePosition(name: "dev", recommendedRating: 2.0)
+		position = new EmployeePosition(name: "dev", recommendedRating: 2.0)
 		higher = new Employee(username: "higher", password: "pass", enabled: true, accountExpired: false,
 			accountLocked: false, passwordExpired: false, position: position)
 		mid = new Employee(username: "mid", password: "pass", enabled: true, accountExpired: false,
@@ -21,6 +22,7 @@ class RatingServiceTests extends GrailsUnitTestCase {
 		lower = new Employee(username: "lower", password: "pass", enabled: true, accountExpired: false, 
             accountLocked: false, passwordExpired: false, position: position, mentor: mid)
         Project project = new Project(name: "project", lead: higher)
+        mockDomain(Employee, [higher, mid, lower])
         mockDomain(Project, [project])
         project.addToCollaborators(mid)
         project.addToCollaborators(lower)
@@ -40,5 +42,50 @@ class RatingServiceTests extends GrailsUnitTestCase {
 	
 	void testTechLeadOnIsTechLeadReturnsTrue(){
 		assertTrue(ratingService.isTechLead(higher, lower))
+	}
+	
+	void testNotTechLeadOnIsTechLeadReturnsFalse(){
+		assertFalse(ratingService.isTechLead(mid, lower))
+	}
+	
+	void testSameEmployeeOnCanRateEmployeeReturnsTrue(){
+		assertTrue(ratingService.canRateEmployee(mid, mid))
+	}
+	
+	void testMentorOnCanRateEmployeeReturnsTrue(){
+		assertTrue(ratingService.canRateEmployee(mid, lower))
+	}
+	
+	void testTechLeadOnCanRateEmployeeReturnsTrue(){
+		assertTrue(ratingService.canRateEmployee(higher, lower))
+	}
+	
+	void testCannotRateOnCanRateEmployeeReturnsFalse(){
+		assertFalse(ratingService.canRateEmployee(mid, higher))
+	}
+	
+	void testGetMenteesReturnsMentees(){
+		Employee newEmp = new Employee(username: "new", password: "pass", enabled: true, accountExpired: false, 
+            accountLocked: false, passwordExpired: false, position: position, mentor: higher)
+        mockDomain(Employee, [higher, lower, mid, newEmp])           
+        def mentees = ratingService.getMentees(higher)
+        assertEquals(2,mentees.size())
+        assertTrue(mentees.contains(newEmp))
+        assertTrue(mentees.contains(mid))
+	}
+	
+	void testGetCollaboratorsReturnsCollaborators(){
+		def collabs = ratingService.getCollaboratorsUnder(higher)
+        assertEquals(2,collabs.size())
+        assertTrue(collabs.contains(lower))
+        assertTrue(collabs.contains(mid))
+	}
+	
+	void testGetEmployeesThatCanBeRatedReturnsEmployeesThatCanBeRated(){
+		def ratees = ratingService.getEmployeesThatCanBeRated(higher)
+		assertEquals(3, ratees.size)
+		assertTrue(ratees.contains(higher))
+        assertTrue(ratees.contains(lower))
+        assertTrue(ratees.contains(mid))
 	}
 }
