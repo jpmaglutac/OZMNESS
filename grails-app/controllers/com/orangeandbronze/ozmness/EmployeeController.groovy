3cffa121a74a7ce4638a06020124fd141aa6efcc
Mickey Espiritu <abraham.espiritu@orangeandbronze.com>
2011-07-18 11:09:04 +0800

    placed scaffolds on some of the controllers

--- grails-app/controllers/com/orangeandbronze/ozmness/EmployeeController.groovy
+++ grails-app/controllers/com/orangeandbronze/ozmness/EmployeeController.groovy
@@ -8,127 +8,65 @@ class EmployeeController {
 
     static allowedMethods = [save: "POST", update: "POST"]
 
-    def index = {
-        redirect(action: "list", params: params)
-    }
-
-    def list = {
-        params.max = Math.min(params.max ? params.int('max') : 10, 100)
-        [employeeInstanceList: Employee.list(params), employeeInstanceTotal: Employee.count()]
-    }
-
-    def create = {
-        def employeeInstance = new Employee()
-        employeeInstance.properties = params
-        return [employeeInstance: employeeInstance]
-    }
-
-    def save = {
+	def scaffold = true
+		
+	def save = {
 		params.password = springSecurityService.encodePassword(params.password)
-        def employeeInstance = new Employee(params)		
+		def employeeInstance = new Employee(params)
 		if (employeeInstance.save(flush: true)) {
 			UserRole.create(employeeInstance, Role.findByAuthority("ROLE_DEV"))
 			flash.message = "${message(code: 'default.created.message', args: [message(code: 'employee.label', default: 'Employee'), '\"' + employeeInstance.username + '\"'])}"
-            redirect(action: "show", id: employeeInstance.id)
-        }
-        else {
-            render(view: "create", model: [employeeInstance: employeeInstance])
-        }
-    }
+			redirect(action: "show", id: employeeInstance.id)
+		}
+		else {
+			render(view: "create", model: [employeeInstance: employeeInstance])
+		}
+	}
 
-    def show = {
-        def employeeInstance = Employee.get(params.id)
-        if (!employeeInstance) {
-            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'employee.label', default: 'Employee'), params.id])}"
-            redirect(action: "list")
-        }
-        else {
-            [employeeInstance: employeeInstance]
-        }
-    }
-
-    def edit = {
-        def employeeInstance = Employee.get(params.id)
-        if (!employeeInstance) {
-            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'employee.label', default: 'Employee'), params.id])}"
-            redirect(action: "list")
-        }
-        else {
+	def edit = {
+		def employeeInstance = Employee.get(params.id)
+		if (!employeeInstance) {
+			flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'employee.label', default: 'Employee'), params.id])}"
+			redirect(action: "list")
+		}
+		else {
 			if(SpringSecurityUtils.ifAllGranted("ROLE_ADMIN")) {
 				def possibleMentors = Employee.list()
 				if(!possibleMentors.isEmpty()){
 					possibleMentors.minus(employeeInstance)
 				}
-            	return [employeeInstance: employeeInstance, possibleMentors: possibleMentors]
+				return [employeeInstance: employeeInstance, possibleMentors: possibleMentors]
 			
 			} else {
 				flash.message = "You are not authorized to edit employees!"
 				redirect(action: "list")
 			}
-        }
-    }
+		}
+	}
 
-    def update = {
-        def employeeInstance = Employee.get(params.id)
-        if (employeeInstance) {
-            if (params.version) {
-                def version = params.version.toLong()
-                if (employeeInstance.version > version) {
-                    
-                    employeeInstance.errors.rejectValue("version", "default.optimistic.locking.failure", [message(code: 'employee.label', default: 'Employee')] as Object[], "Another user has updated this Employee while you were editing")
-                    render(view: "edit", model: [employeeInstance: employeeInstance])
-                    return
-                }
-            }
-			params.password = springSecurityService.encodePassword(params.password)
-            employeeInstance.properties = params
-            if (!employeeInstance.hasErrors() && employeeInstance.save(flush: true)) {
-                flash.message = "${message(code: 'default.updated.message', args: [message(code: 'employee.label', default: 'Employee'), employeeInstance.username])}"
-                redirect(action: "show", id: employeeInstance.id)
-            }
-            else {
-                render(view: "edit", model: [employeeInstance: employeeInstance])
-            }
-        }
-        else {
-            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'employee.label', default: 'Employee'), params.id])}"
-            redirect(action: "list")
-        }
-    }
-
-    def delete = {
-        def employeeInstance = Employee.get(params.id)
-        if (employeeInstance) {
-            try {
+	def delete = {
+		def employeeInstance = Employee.get(params.id)
+		if (employeeInstance) {
+			try {
 				if(SpringSecurityUtils.ifAllGranted("ROLE_ADMIN")) {
-	            	Employee.findAllByMentor(employeeInstance).each {
-	            		it.mentor = null
-	            		it.save(flush: true)
-	            	}
-	            	Project.list().each {
-	            		it.removeFromCollaborators(employeeInstance)
-	            	}
-	            	Rating.findAllByEmployeeRated(employeeInstance).each {
-	            		it.delete(flush: true)
-	            	}
-	            	UserRole.removeAll(employeeInstance)
-	                employeeInstance.delete(flush: true)
-	                flash.message = "Employee has been deleted."
-	                redirect(action: "list")
+					employeeInstance.enabled = false
+					employeeInstance.save(flush: true)
+					flash.message = "Employee's account has been disabled."
+					redirect(action: "list")
 				} else {
 					flash.message = "You are not authorized to delete employees!"
 					redirect(action: "list")
 				}
-            }
-            catch (org.springframework.dao.DataIntegrityViolationException e) {
-                flash.message = "${message(code: 'default.not.deleted.message', args: [message(code: 'employee.label', default: 'Employee'), params.id])}"
-                redirect(action: "show", id: params.id)
-            }
-        } else {
-            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'employee.label', default: 'Employee'), params.id])}"
-            redirect(action: "list")
-        }
-    }
+			}
+			catch (org.springframework.dao.DataIntegrityViolationException e) {
+				flash.message = "${message(code: 'default.not.deleted.message', args: [message(code: 'employee.label', default: 'Employee'), params.id])}"
+				redirect(action: "show", id: params.id)
+			}
+		} else {
+			flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'employee.label', default: 'Employee'), params.id])}"
+			redirect(action: "list")
+		}
+	}
 	
 	def showEmployeeRatings ={
 		def employeeInstance = Employee.get(params.id)
